59e7f0617b2f14485a21af754f240eaa72bd729b
alexcher-im <43251717+alexcher-im@users.noreply.github.com>
2022-06-15 22:09:25 +0300

    Started adding support for ESP8266

--- main/platform/api.h
+++ main/platform/api.h
@@ -1,7 +1,22 @@
 #pragma once
 
-#ifdef ESP_PLATFORM
+// checking for ESP-IDF
+#if defined(ESP_PLATFORM)
+#include <esp_system.h>
+
+// using ESP32 api
+#if defined(CONFIG_IDF_TARGET_ESP32)
 #include "esp32_api.h"
-#else
-#include "common_api.h"
+// using ESP8266 api
+#elif defined(CONFIG_IDF_TARGET_ESP8266)
+#include "esp8266_api.h"
+
 #endif
+
+// not an ESP-IDF context
+#else
+// using common (PC) api
+#include "common_api.h"
+
+#endif
+
